3effb8a1f988b563e94d2297ef576111a84bc795
CielQian <qianhong91@outlook.com>
2019-02-20 17:35:37 +0800

    调整详情

--- src/components/LinkDetail.vue
+++ src/components/LinkDetail.vue
@@ -134,6 +134,12 @@ export default {
         _this.link.folderId = "未归档";
         _this.selectedTag = [];
         _this.unSelectedTag = [..._this.tags];
+      }).error(err => {
+          _this.analysising = false;
+          _this.analysisHolder = '解析失败，请手动完善标题...';
+          _this.link.folderId = "未归档";
+          _this.selectedTag = [];
+          _this.unSelectedTag = [..._this.tags];
       });
     },
     saveLink: function(cb) {
