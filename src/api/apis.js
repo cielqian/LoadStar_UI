650613c4061e7765a8807fbec62a79baa0523261
CielQian <qianhong91@outlook.com>
2019-05-31 14:59:51 +0800

    修改生产环境接口地址

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,5 +1,5 @@
 export default{
-    baseHost: process.env.NODE_ENV == 'production'?  'http://139.9.234.51:9080' : 'http://localhost:9080',
+    baseHost: process.env.NODE_ENV == 'production'?  'https://loadstar.top' : 'http://localhost:9080',
     // baseHost:'http://loadstar.top:9080',
     auth:{
         signUp:'/user-service/api/account',
